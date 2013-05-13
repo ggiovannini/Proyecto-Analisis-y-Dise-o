2a90e159cdfec444956819e5c336a315281f4d2c
Bruno Isoardi <brunoiso_2@hotmail.com>
2013-05-13 01:47:27 -0300

    Clases Estate y Owner agregadas

--- proyecto/config/schema.sql
+++ proyecto/config/schema.sql
@@ -5,3 +5,21 @@ CREATE TABLE users(
   first_name VARCHAR(56),
   last_name VARCHAR(56)
 );
+
+DROP TABLE IF EXISTS estates;
+CREATE TABLE estates(
+  id  int(11) NOT NULL  auto_increment PRIMARY KEY,
+  name VARCHAR(60),
+  dir VARCHAR(60),
+  phone VARCHAR(15),
+  mail VARCHAR (40),
+  web_site VARCHAR (40)
+);
+
+DROP TABLE IF EXISTS owners;
+CREATE TABLE owners(
+  id  int(11) NOT NULL  auto_increment PRIMARY KEY,
+  name VARCHAR(60),
+  dir VARCHAR(60),
+  email VARCHAR(40)
+);
--- proyecto/src/main/java/com/unrc/app/Inmo.java
+++ proyecto/src/main/java/com/unrc/app/Inmo.java
@@ -1,5 +1,7 @@
 package com.unrc.app;
 
+import com.unrc.app.models.Estate;
+import com.unrc.app.models.Owner;
 import com.unrc.app.models.User;
 
 import org.javalite.activejdbc.Base;
@@ -9,14 +11,18 @@ import org.slf4j.LoggerFactory;
 public class Inmo {
     public static void main( String[] args )
     {
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "");
-
-        User e = new User();
-        e.set("email", "user@email.com");
-        e.set("first_name", "John");
-        e.set("last_name", "Doe");
-        e.saveIt();
-
+        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+        
+          Owner o = new Owner();
+          o.set("email", "brunoiso_2@hotmail.com");
+          o.saveIt();
+//        User e = new User();
+//        e.set("email", "brunoiso@hotmail.com");
+//        e.set("first_name", "Bruno");
+//        e.set("last_name", "Isoardi");
+//        e.saveIt();
+        
         System.out.println( "Hello World!" );
+ 
     }
 }
